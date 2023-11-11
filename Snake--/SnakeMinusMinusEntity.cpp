ce235e8fe32e9fea1915e5d9536590b6c083bd33
FZF-Dev <fzoltan123456@gmail.com>
2023-11-11 23:23:11 +0100

    Add base for entity class

---
+++ Snake--/SnakeMinusMinusEntity.cpp
@@ -0,0 +1,1 @@
+#include "SnakeMinusMinusEntity.h"
---
+++ Snake--/SnakeMinusMinusEntity.h
@@ -0,0 +1,19 @@
+#ifndef SNAKE_MINUS_MINUS_ENTITY
+#define SNAKE_MINUS_MINUS_ENTITY
+
+
+class SnakeMinusMinusEntity
+{
+private:
+	int x;
+	int y;
+	char symbol;
+
+	SnakeMinusMinusEntity();
+
+public:
+	SnakeMinusMinusEntity(const int x, const int y, const char symbol);
+};
+
+
+#endif
